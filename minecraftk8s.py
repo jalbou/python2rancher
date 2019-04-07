2d9f24e0dbd1e9ce24d018184f37fe968b8abf96
Jeremy Albou <jeremy@chamaa.local>
2019-04-07 17:17:42 -0400

    updated setNewWorkload function

--- minecraftk8s.py
+++ minecraftk8s.py
@@ -5,19 +5,30 @@ import json
 import urllib3
 def setNewWorkload(workloadName,projectID):
     url = "https://192.168.55.107/v3/project/"+projectID+"/workloads"
-    json_file=open("workloadConfig.json", "r")
-    rawPayload = json_file.read()
-    payload = json.loads(rawPayload)
+    # Read new Workload JSON config file
+    with open('workloadConfig.json', 'r') as f:
+        rawJSON = json.load(f)
+    #Set JSON config file according workload arguments
     payload['containers'][0]['name'] = workloadName
-## Formating JSON payload from arguments
-    print(payload['containers'][0]['name'])
+    payload['containers'][0]['ports'][0]['dnsName'] = workloadName+'-nodeport'
+    payload['selector']['matchLabels']['workload.user.cattle.io/workloadselector'] = "statefulSet-default-"+workloadName
+    payload['name']= workloadName
+    payload['workloadLabels']['workload.user.cattle.io/workloadselector'] = 'statefulSet-default-'+workloadName
+    payload['labels']['workload.user.cattle.io/workloadselector'] = 'statefulSet-default-'+workloadName
+    payload['workload.user.cattle.io/workloadselector'] = 'statefulSet-default-'+workloadName
+    payload['statefulSetConfig']['serviceName'] = workloadName
+    payload['projectId'] = projectID
+    payload=json.dumps(rawJSON, indent=4, sort_keys=True)
+    #Set HTTP Header
     headers = {
-    'Content-Type': "application/json",
-    'Authorization': "Basic dG9rZW4tbHFyZHg6NXE2emxmd21iNHF4YjZydm5nOThoazg3N2ZxZGJrNm01Nmt2ZjdwZGRnNGxtbWtjNGpnOHF3",
-    'cache-control': "no-cache",
-    'Postman-Token': "3925edcb-d2cb-4477-8303-5094ba36886b"
+        'Content-Type': "application/json",
+        'Authorization': "Basic dG9rZW4tbHFyZHg6NXE2emxmd21iNHF4YjZydm5nOThoazg3N2ZxZGJrNm01Nmt2ZjdwZGRnNGxtbWtjNGpnOHF3",
+        'cache-control': "no-cache",
+        'Postman-Token': "3925edcb-d2cb-4477-8303-5094ba36886b"
     }
     response = requests.request("POST", url, data=payload, headers=headers,verify=False)
+    if (response)
+    print(response.text)
 
 def getWorkload(workloadName,projectID):
     url = "https://192.168.55.107/v3/project/"+projectID+"/workloads/statefulset:default:"+workloadName 
