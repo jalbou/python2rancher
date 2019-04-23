a743711b211d1338b01c879b16fb4d9875bff5ea
jeremy <jalbou@me.com>
2019-04-22 21:40:06 -0400

    quick fix on env script

--- setUserEnv.ps1
+++ setUserEnv.ps1
@@ -1,11 +1,3 @@
-#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-#choco install vscode
-#choco install git
-#choco install postman
-#choco install notepadplusplus.install
-#choco install python3
-
-Set-ExecutionPolicy RemoteSigned
 $RancherAuth = Read-Host -Prompt 'Enter your Rancher API Bearer'
 $RancherToken = Read-Host -Prompt 'Enter your Rancher API Token'
 [Environment]::SetEnvironmentVariable("RancherAuth","Basic "+$RancherAuth,"user")
