5974160556770f4cc7c1935a9dfdde52e7ec84a7
jeremy <jalbou@me.com>
2019-04-25 11:15:57 -0400

    fix create workload function

--- webservice.py
+++ webservice.py
@@ -4,11 +4,10 @@ app = Flask(__name__)
 
 @app.route("/create")
 def create():
-    return workload.create('web',2)
+    return workload.create('web')
 @app.route("/remove")
 def remove():
     workload.remove('minecraft1')
-
-
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000',debug=True)
--- workloadfunc.py
+++ workloadfunc.py
@@ -11,8 +11,8 @@ import os
 import re
 urllib3.disable_warnings()
 
-def create(workloadName,count):
-#Get Rancher API cred from system variable enviroments
+def create(workloadName):
+    #Get Rancher API cred from system variable enviroments
     headers = {
         'Content-Type': "application/json",
         'Authorization': os.environ.get('RancherAuth'),
@@ -26,8 +26,7 @@ def create(workloadName,count):
         'workloadTemplate':workloadName,
         'headers': headers
     }
-       #Set HTTP Header for Rancher API Calls
-    i=0
+    #Set HTTP Header for Rancher API Calls
     #Get A list of actual workload matching the template workload requested using a regex filter
     workloads = rancher.getAllWorkloadName(RancherObj)
     regex = re.compile(RancherObj['workloadTemplate'])
@@ -37,24 +36,21 @@ def create(workloadName,count):
         firstOccurence = 0
     else:
         firstOccurence =int(re.findall(r'\d', str(filteredList[0]))[0])
-    while i < count:
-        i += 1
-        workload = workloadName+str(i+firstOccurence)
-        #print(firstOccurence)
-        isWorkload = rancher.getWorkload(RancherObj,workload)
-        isStorageClass = rancher.getStorageClass(RancherObj)
-        if isWorkload.status_code == 404 or isStorageClass==404:
-            if  isStorageClass == 404:
-                print('Storage Class storageclass'+workloadName+' not found , creating ... ')
-                rancher.setNewStorageClass(RancherObj)
-            if  isWorkload.status_code == 404:
-                print('Workload '+workload+' not found , creating ... ')
-                rancher.setNewPVC(RancherObj,workload)
-                rancher.setNewWorkload(RancherObj,workload)
-                newWorkload = rancher.getWorkload(RancherObj,workload)
-                return workloadName+"created, the service is available on "+(str(json.loads(newWorkload.content)))
-            else:
-                print("Workload "+workload+" already existing. Escaping...")
+    workload = workloadName+str(firstOccurence+1)
+    isWorkload = rancher.getWorkload(RancherObj,workload)
+    isStorageClass = rancher.getStorageClass(RancherObj)
+    if isWorkload.status_code == 404 or isStorageClass==404:
+        if  isStorageClass == 404:
+            print('Storage Class storageclass'+workloadName+' not found , creating ... ')
+            rancher.setNewStorageClass(RancherObj)
+        if  isWorkload.status_code == 404:
+            print('Workload '+workload+' not found , creating ... ')
+            rancher.setNewPVC(RancherObj,workload)
+            rancher.setNewWorkload(RancherObj,workload)
+            newWorkload = rancher.getWorkload(RancherObj,workload)
+            return workloadName+"created, the service is available on "+(str(json.loads(newWorkload.content)))
+        else:
+            print("Workload "+workload+" already existing. Escaping...")
                 
 def remove(workloadName):
     #Get Rancher API cred from system variable enviroments
